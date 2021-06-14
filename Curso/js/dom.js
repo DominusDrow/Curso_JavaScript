0ec98c859361ce3e9d6436a3a167c97861daf194
DominusDrow <galacticovasquez@gmail.com>
2021-06-14 16:14:53 -0500

    nodos del DOM

--- Curso/js/dom.js
+++ Curso/js/dom.js
@@ -23,6 +23,31 @@ console.log(document.images);
 console.log(document.forms);
 console.log(document.styleSheets);  //obtener las hojas de estilo
 console.log(document.scripts);      //obtener los archivos javascript
-document.write("<h2>Hola soy dinamico xd</h2>");    //escribe dinamicamente en el document
+//document.write("<h2>Hola soy dinamico xd</h2>");    //escribe dinamicamente en el document
+console.log("\n");
+
+//NODOS 
+
+//los mas importantes son los nodos de tipo elemento y  los de tipo texto 
+//Node.ELEMENT_NODE Y Node.ELEMENT_TEXT
+
+//metodos obsoletos 
+console.log(document.getElementsByTagName("li")); //resive cualquier etiqueta (li, head, figure...)
+console.log(document.getElementsByClassName("card"));
+console.log(document.getElementsByName("nombre"));
+
+//metodos por los que han sido remplazados
+console.log(document.querySelector("#name"));    //# para id y . para clases
+console.log(document.querySelectorAll("a"));    //para seleccionar todos, el anterior solo uno
+
+// sigue teniendo mejor rendimiento que querySelector
+console.log(document.getElementById("menu"));
+
+//los NodeList y HTMLCollection comparten algunos elementos con los arreglos
+console.log(document.querySelectorAll("a").length);
+document.querySelectorAll("a").forEach(e => console.log(e));
+console.log(document.querySelectorAll(".card")[2]); //se puede usar la notacion de arreglos
+
+console.log(document.querySelectorAll("#menu li")); //trae solo las li dentro de menu
 
 
