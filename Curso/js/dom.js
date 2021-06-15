af14b2d073cc6fcb4426d97676f862e53f07ec33
DominusDrow <galacticovasquez@gmail.com>
2021-06-15 17:22:24 -0500

    Attributes y Data-Attributes

--- Curso/js/dom.js
+++ Curso/js/dom.js
@@ -26,6 +26,9 @@ console.log(document.scripts);      //obtener los archivos javascript
 //document.write("<h2>Hola soy dinamico xd</h2>");    //escribe dinamicamente en el document
 console.log("\n");
 
+
+
+
 //NODOS 
 
 //los mas importantes son los nodos de tipo elemento y  los de tipo texto 
@@ -49,5 +52,57 @@ document.querySelectorAll("a").forEach(e => console.log(e));
 console.log(document.querySelectorAll(".card")[2]); //se puede usar la notacion de arreglos
 
 console.log(document.querySelectorAll("#menu li")); //trae solo las li dentro de menu
+
+
+
+//ATRIBUTOS
+
+//los atributos son los id, class, href, lang , etc....
+
+//se puede acceder atravez de la notacion del punto
+console.log(document.documentElement.lang);
+console.log(document.querySelector(".link-dom").href);      //devuelve toda la direccion
+
+//lo correcto es utilizar los metodos que devuelven el atributo absoluto
+console.log(document.documentElement.getAttribute("lang"));
+console.log(document.querySelector(".link-dom").getAttribute("href"));  //devuelve direccion absoluta
+
+//tambien se pueden cambiar los atrubutos dinamicamente
+
+//notacion del punto
+document.documentElement.lang="es";
+console.log(document.documentElement.lang);
+
+//con metodo setAtribute
+document.documentElement.setAttribute("lang","es-MX");
+console.log(document.documentElement.getAttribute("lang"));     
+
+//tambien podemos crear Data-Attributes
+document.getElementById("texto").setAttribute("Data-ej","hola xd");
+console.log(document.querySelector("#texto"));
+
+//pra consultar Data-Attributes con la notacion del punto
+console.log(document.getElementById("texto").dataset);      //devuelve un map con todos
+console.log(document.getElementById("texto").dataset.ej);   //acceder a uno en particular
+
+//podemos guardar elementos del DOM en variables
+//pueden ser const ya que son similares a objetos (nodos)
+//es buena practica declararlos con $
+const $linkDOM = document.querySelector(".link-dom");
+console.log($linkDOM);
+
+//se pueden hacer operaciones basicas y mayor legibilidad
+console.log($linkDOM.hasAttribute("class"));
+
+console.log(document.querySelector("#texto").hasAttribute("Data-ej"));
+document.querySelector("#texto").removeAttribute("data-ej");
+console.log(document.querySelector("#texto").hasAttribute("Data-ej"));
+
+//podemos hacer que el enlace se abra en otra pestaña
+$linkDOM.setAttribute("target","_blank");
+$linkDOM.setAttribute("rel","noopener");    //buena practica por seguridad
+
+//cambiar el enlace 
+$linkDOM.setAttribute("href","http://youtube.com");
 
 
