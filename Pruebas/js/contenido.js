7ae68e420d29426f9785818f13f861d0ffa2e01d
DominusDrow <galacticovasquez@gmail.com>
2021-06-16 17:28:31 -0500

    html basico

--- Pruebas/js/contenido.js
+++ Pruebas/js/contenido.js
@@ -2,10 +2,19 @@ alert("Hola mundo");
 
 function sumar() {
 	
-	var elem1= document.getElemntById("num1").value;
-	var elem2= document.getElemntById("num2").value;
+	var elem1= document.getElementById("num1").value;
+	var elem2= document.getElementById("num2").value;
 	var suma = parsInt(elem1)+parsInt(elem2);
 
 	alert("la suma es "+suma);
 
 }
+
+let boton = document.querySelector("");
+
+console.log(boton);
+
+console.log(document.getElementById("num1").value);
+
+
+parseInt("3");
