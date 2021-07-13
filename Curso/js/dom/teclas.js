61fd28d1a4060ff2e405e26a611b6e9f9ed6dbe6
DominusDrow <galacticovasquez@gmail.com>
2021-07-13 01:02:03 -0500

    shortCuts

--- Curso/js/dom/teclas.js
+++ Curso/js/dom/teclas.js
@@ -1,34 +1,34 @@
-export default function moveBall(ball){
+
+export function moveBall(ball,e){
     const d = document,
         $ball = d.querySelector(ball);
 
     $ball.style.left = "0rem";
     $ball.style.top = "0rem";
 
+    if(e.keyCode === 40){
+        $ball.style.top = `${parseInt($ball.style.top)+1}rem`;
 
-    d.addEventListener("keydown", (e) => {
+    }
+    else if(e.keyCode === 38){
+        $ball.style.top = `${parseInt($ball.style.top)-1}rem`;
+    }
+    else if(e.keyCode === 39){
+        $ball.style.left = `${parseInt($ball.style.left)+1}rem`;
+    }
+    else if(e.keyCode === 37){
+        $ball.style.left = `${parseInt($ball.style.left)-1}rem`;
+    }
 
-        if(e.keyCode === 40){
-            $ball.style.top = `${parseInt($ball.style.top)+1}rem`;
+      
 
-        }
-        else if(e.keyCode === 38){
-            $ball.style.top = `${parseInt($ball.style.top)-1}rem`;
-        }
-        else if(e.keyCode === 39){
-            $ball.style.left = `${parseInt($ball.style.left)+1}rem`;
-        }
-        else if(e.keyCode === 37){
-            $ball.style.left = `${parseInt($ball.style.left)-1}rem`;
-        }
+} 
 
-        if(e.altKey && e.keyCode === 65)
-            alert();
-        else if(e.altKey && e.keyCode === 80)
-            prompt();
-        else if(e.altKey && e.keyCode === 67)
-            confirm();
-        
-
-    })
+export function shortCuts(e) {
+    if(e.altKey && e.key === "a")
+        alert("has mandado una alerta");
+    else if(e.altKey && e.key === "p")
+        prompt("has mandado un aviso");
+    else if(e.altKey && e.key === "c")
+        confirm("has mandado una confirmación");
 }
--- Curso/js/ejercicios_dom.js
+++ Curso/js/ejercicios_dom.js
@@ -1,6 +1,6 @@
 import hamburgerMenu from "./dom/menu_Hamburguesa.js"; 
 import {relojDijital,alarmaDijital} from "./dom/relog.js";
-import teclas from "./dom/teclas.js";
+import {moveBall, shortCuts} from "./dom/teclas.js";
 
 const d = document;
 
@@ -10,9 +10,15 @@ d.addEventListener("DOMContentLoaded", e => {
     relojDijital(".clock","#btn-clock","#btn-clock-off","clock-on");
     alarmaDijital("/Curso/src/sonido-de-alarma.mp3","#btn-alarm","#btn-alarm-off");
 
-    teclas(".ball");
 
 })
+
+//los eventos de teclado deben desencadenarse directamente del archivo padre
+d.addEventListener("keydown", (e) => {
+    shortCuts(e);
+    moveBall(".ball",e);
+})
+
 
 
 
