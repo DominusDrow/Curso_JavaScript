f7ba367f8748b6640249f6a645761811933965fc
DominusDrow <galacticovasquez@gmail.com>
2021-08-16 17:19:01 -0500

    CountDown finish

--- Curso/js/dom/countdown.js
+++ Curso/js/dom/countdown.js
@@ -7,11 +7,17 @@ export default function countdown(dateDown,count,msg){
     let countInterval = setInterval(() => {
         let lCount = dateDown.getTime() - new Date().getTime(),
         days = Math.floor(lCount/(1000*60*60*24)),
-        hours = new Date(lCount).getUTCHours(),
-        miinutes = new Date(lCount).getUTCMinutes(),
-        seconds = new Date(lCount).getUTCSeconds();
+        hours = ("0"+ new Date(lCount).getUTCHours()).slice(-2),        //slice permite corta una cadana 
+        miinutes = ("0"+ new Date(lCount).getUTCMinutes()).slice(-2),   //se le agrega un cero para que sean 2 dijitos
+        seconds = ("0"+ Math.floor((lCount % (1000*60))/1000)).slice(-2);       //OTRA FORMA DE HACERLO 
 
         $count.textContent = `Faltan ${days} dias ${hours} horas ${miinutes} minutos ${seconds} segundos`;
+        
+        if(lCount<0){
+            clearInterval(countInterval);
+            $count.textContent = msg;
+        } 
+
     }, 1000);
 
 
