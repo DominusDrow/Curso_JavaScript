8ba6cfbf5c2cd9f89193865cb0ebe54e88a24121
DominusDrow <galacticovasquez@gmail.com>
2021-09-03 17:54:58 -0500

    responposive tester

---
+++ Curso/js/dom/responsiveTester.js
@@ -0,0 +1,7 @@
+
+const d = document,
+    w = window;
+
+export default function responsiveTester (){
+
+}
--- Curso/js/ejercicios_dom.js
+++ Curso/js/ejercicios_dom.js
@@ -23,8 +23,14 @@ d.addEventListener("DOMContentLoaded", e => {
     resposiveJs(
         "youtube",
         "(min-width: 1024px)",
-        `<iframe width="560" height="315" src="https://www.youtube.com/embed/6IwUl-4pAzc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        `<iframe width="560" height="315" src="https://www.youtube.com/embed/6IwUl-4pAzc" frameborder="0" allowfullscreen></iframe>`,
         `<a href="https://youtu.be/6IwUl-4pAzc">Ver video</a>`
+    )
+    resposiveJs(
+        "gmaps",
+        "(min-width: 1024px)",
+        `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2056.307505136797!2d-98.17600658149075!3d19.05122439987323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc055a9e73013%3A0x1656bb12d5450d7d!2sPuebla%2C%2072270%20Puebla%2C%20Pue.!5e0!3m2!1ses-419!2smx!4v1630709384326!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" loading="lazy"></iframe>`,
+        `<a href="https://goo.gl/maps/9W1hyBu7hatqU2bG6">Ver mapa</a>`
     )
 })
 
