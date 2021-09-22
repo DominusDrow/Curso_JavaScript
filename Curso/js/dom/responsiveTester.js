6cdb397116eebe375cc57600d1cad1e714f5a085
DominusDrow <galacticovasquez@gmail.com>
2021-09-22 18:19:08 -0500

    responsive tester completo

--- Curso/js/dom/responsiveTester.js
+++ Curso/js/dom/responsiveTester.js
@@ -2,21 +2,25 @@
 const d = document,
     w = window;
 
-export default function responsiveTester (form,site,w,h){
+export default function responsiveTester (form,site,withV,high){
     const $form = d.querySelector(form),
         $site = d.getElementById(site),
-        $with = d.getElementById(w),
-        $high = d.getElementById(h);
+        $with = d.getElementById(withV),
+        $high = d.getElementById(high);
 
-    let window;
+    let windowO;
 
     d.addEventListener("submit", e => {
         if(e.target.matches(form)){
-           window =  w.open($site.textContent);
-           
+           windowO =  w.open($site.value,"",`width=${$with.value},height=${$high.value}`);
         }
     })
 
-    
+    d.addEventListener("reset", e => {
+        if(e.target.matches(form)){
+            windowO.close();
+        }
+    })
+
 
 }
