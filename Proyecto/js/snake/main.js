222950ce276a6e280dde97504cd81684debdedad
DominusDrow <galacticovasquez@gmail.com>
2022-07-07 10:32:39 -0500

    food snake

--- Proyecto/js/snake/main.js
+++ Proyecto/js/snake/main.js
@@ -8,12 +8,13 @@ const snake = [
 	{x:20, y:20, d:1},
 	{x:40, y:20, d:1},
 	{x:60, y:20, d:1},
-	{x:80, y:20, d:1},
-	{x:100, y:20, d:1},
 ]
 
 //direccion que va tomando la serpiente
 let direction = 1;
+
+//coordenadas de la cominda de la serpiente
+let foodX = 30, foodY = 30;
 
 d.addEventListener("DOMContentLoaded", () => {
 	init();
@@ -35,6 +36,7 @@ const init = () => {
 	ctx.fillRect(0,0,$snake.width,$snake.height);
 
 	drawSnake();
+	foodSnake();
 
 }
 
@@ -49,6 +51,19 @@ const ereaseSnake = () =>{
 	ctx.fillStyle = "#042001"
 	for(let el  of snake){
 		ctx.fillRect(el.x,el.y,20,20);
+	}
+}
+
+const foodSnake = () => {
+	ramdomCoordinates();	
+	ctx.fillStyle = "#C0392B"
+	ctx.fillRect(foodX,foodY,20,20);
+}
+
+const ramdomCoordinates = () => {
+	while((foodX % 20 != 0) && (foodY % 20 != 0)){
+		foodX = Math.floor(Math.random() * $snake.width);
+		foodY = Math.floor(Math.random() * $snake.height);
 	}
 }
 
@@ -73,20 +88,25 @@ const playSnake = () => {
 
 		});
 
-	drawSnake();
-	},300)
+		if(snake.at(-1).x === foodX && snake.at(-1).y === foodY){
+			foodSnake();
+
+		}
+
+		drawSnake();
+	},200)
 }
 
 
 const moveSnake = e => {
 
-  if(e.keyCode === 37)
+  if(e.keyCode === 37 && direction != 1)
 		direction = 3;
-  else if(e.keyCode === 38)
+  else if(e.keyCode === 38 && direction != 2)
 		direction = 4;
-  else if(e.keyCode === 39)
+  else if(e.keyCode === 39 && direction != 3)
 		direction = 1;
-  else if(e.keyCode === 40)
+  else if(e.keyCode === 40 && direction != 4)
 		direction = 2;
 
 }
