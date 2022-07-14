2c8ab23a2c9bb3e6df3dbfd211e72ef171ebf8eb
DominusDrow <galacticovasquez@gmail.com>
2022-07-14 09:23:18 -0500

    aploplay

--- Curso/js/dom/SmartVideo.js
+++ Curso/js/dom/SmartVideo.js
@@ -3,16 +3,32 @@ const d = document;
 
 export default function smartVideo (video){
 
-    const $video = d.querySelector(video);
+    const $video = d.querySelectorAll(video);
 
-    playVideo($video);
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0
+    }
 
+    let observer = new IntersectionObserver(callback, options);
+    
+    observer.observe($video);
+
+
+}
+
+const callback = (entries,observer) => {
+  entries.forEach(entry => {
+      playVideo(entry);
+
+  })
 }
 
 async function playVideo(videoElem) {
   try {
     await videoElem.play();
   } catch(err) {
-      alert("No se pudo reproducir el video");
+      alert("Active los permisos de autoplay");
   }
 }
