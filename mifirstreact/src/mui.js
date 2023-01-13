65ff02b4d6aff3b0c2b6948e212d0932c759f767
DominusDrow <galacticovasquez@gmail.com>
2023-01-13 16:18:53 -0600

    use react hooks to paint users in screen

--- mifirstreact/src/mui.js
+++ mifirstreact/src/mui.js
@@ -1,26 +1,41 @@
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import { useState, useEffect } from "react";
 
-export const GetAvatar = async () => {
+export const GetAvatar = () => {
 
+	//const [data, setData] = useState([]);
+	let data = [];
+
+	useEffect(() => {
+		getData().then(users => data = users);
+	}, []);
+
+
+	setTimeout(() => {
+		{data.map(user => {
+			{console.log(user)}
+			return <Stack direction="row" spacing={2} key={user.id}>
+				<Avatar alt={user.name} src={`https://robohash.org/user${user.id}`} />
+				<h2>{user.name}</h2>
+			</Stack>
+		})}
+
+	}, 1000);
+}
+
+async function getData() {
 	try {
 		const response = await fetch('https://jsonplaceholder.typicode.com/users'),
 			data = await response.json();
 
 		if(!response.ok) throw new Error(data.message);
 
-		const users = Object.values(data);
+		return Object.values(data);
 
 	}
 	catch (error) {
 		console.log(error);
 	}
-
-	return <>
-		<Stack direction="row" spacing={2}>
-			<Avatar alt="Remy Sharp" src="http://placeimg.com/200/200/people" />
-		</Stack>
-	</>;
-
 
 }
