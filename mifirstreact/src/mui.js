e9f5bbf69588d2fc8be8a5a9d5bb4a2acb5fa2d1
DominusDrow <galacticovasquez@gmail.com>
2023-01-14 23:54:36 -0600

    try to get api data with axios and react hooks

--- mifirstreact/src/mui.js
+++ mifirstreact/src/mui.js
@@ -1,41 +1,34 @@
+import axios from 'axios';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import { useState, useEffect } from "react";
 
 export const GetAvatar = () => {
 
-	//const [data, setData] = useState([]);
-	let data = [];
+	const [data, setData] = useState({hits: []});
 
 	useEffect(() => {
-		getData().then(users => data = users);
+		const fetchData = async () => {
+			const result = await axios('https://jsonplaceholder.typicode.com/users')
+			setData(result.data);
+			console.log(data);
+		}
+		fetchData();
 	}, []);
 
-
-	setTimeout(() => {
-		{data.map(user => {
-			{console.log(user)}
-			return <Stack direction="row" spacing={2} key={user.id}>
-				<Avatar alt={user.name} src={`https://robohash.org/user${user.id}`} />
-				<h2>{user.name}</h2>
-			</Stack>
-		})}
-
-	}, 1000);
+	return (
+		<div>
+			{data.hits.map(item => (
+				<div key={item.id}>
+					<Stack direction="row" spacing={2}>
+						<Avatar alt={item.name} src={`https://robohash.org/user${item.id}`} />
+						<h2>{item.name}</h2>
+					</Stack>
+				</div>
+			))}
+		</div>
+	);
 }
 
-async function getData() {
-	try {
-		const response = await fetch('https://jsonplaceholder.typicode.com/users'),
-			data = await response.json();
 
-		if(!response.ok) throw new Error(data.message);
 
-		return Object.values(data);
-
-	}
-	catch (error) {
-		console.log(error);
-	}
-
-}
