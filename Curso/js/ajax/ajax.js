44b4280c54205ca97b06c95a28003fce4bed510e
DominusDrow <galacticovasquez@gmail.com>
2023-01-14 23:25:08 -0600

    axios

--- Curso/js/ajax/ajax.js
+++ Curso/js/ajax/ajax.js
@@ -78,3 +78,27 @@
 
     getData();
 })();
+
+(() => {
+    const $axios = document.querySelector(".axios"),
+        $fragment = document.createDocumentFragment();
+
+    axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then((res) => {
+            let json = res.data;
+            json.forEach((el) => {
+                const $li = document.createElement('li');
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+            $axios.appendChild($fragment);
+        })
+        .catch((err) => {
+            let message = err.response.statusText || 'Ocurrió un error';
+            $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+        })
+        .finally(() => {
+            console.log('Esto se ejecutará independientemente del axios');
+        });
+})();
