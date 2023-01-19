2d835f03e600bab36a913d08724948670458b273
DominusDrow <galacticovasquez@gmail.com>
2023-01-18 22:21:41 -0600

    use Contex

---
+++ react_taks_app/src/contex/TaskContex.jsx
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+
+export const TaskContext = createContext();
+
+export const TaskProvider = ({ children }) => {
+  const [tasks, setTasks] = useState(data);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  const addTask = (task) => {
+    setTasks([...tasks, {
+      id: tasks.length + 1,
+      name: task.value,
+      description: task.description
+    }]);
+  };
+
+  const deleteTask = (id) => {
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
+  };
+
+  return (
+    <TaskContext.Provider value={{ tasks, addTask, deleteTask }}>
+      {children}
+    </TaskContext.Provider>
+  );
+}
