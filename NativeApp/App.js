b6410acff534eb7e72387f8e3d49f686528defeb
DominusDrow <galacticovasquez@gmail.com>
2023-02-03 15:17:19 -0600

    to learn about buttons and react in movile

--- NativeApp/App.js
+++ NativeApp/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.canceled === true) 
+      return;
+
+    setSelectedImage({ localUri: pickerResult.assets[0].uri });
+  }
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-      <Image source={{uri:"https://picsum.photos/200/300"}} style={styles.image} />
+      <Text style={styles.title}>Pick an image!</Text>
+      <Image source={{uri:selectedImage.localUri || "https://picsum.photos/200/300"}} style={styles.image} />
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={openImagePickerAsync}>
+        <Text style={styles.buttonText}>Select a image</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -23,7 +48,18 @@ const styles = StyleSheet.create({
   },
   image: {
     width: 200,
-    height: 300,
+    height: 200,
+    borderRadius: 100,
+    resizeMode: 'contain',
+  },
+  button: {
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
   }
 });
 
